753d1fdf3ec4566cc2ef9fec0e9ecdcf96a5ab00
Tim Lau <tlau9195@gmail.com>
2014-07-01 14:20:35 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -11,8 +11,8 @@
 @implementation MainScene
 
 - (void)play {
-    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
-    [[CCDirector sharedDirector] replaceScene:gameplayScene];
+    
+CCLOG(@"play button pressed");
 }
 
 @end
