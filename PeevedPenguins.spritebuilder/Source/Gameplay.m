97595fc18ee52c66cf973ee1e6ad4e4a9f8104f0
Tim Lau <tlau9195@gmail.com>
2014-07-01 16:42:13 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -43,7 +43,7 @@
     [penguin.physicsBody applyForce:force];
     
     // ensure followed object is in visible are when starting
-    self.position = ccp(0, 0);
+    _contentNode.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
     [self runAction:follow];
 }
