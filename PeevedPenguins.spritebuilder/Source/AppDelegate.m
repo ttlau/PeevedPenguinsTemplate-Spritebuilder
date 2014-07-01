cd77c1c05f7718974a7590c693ab33eb3bedaf04
Tim Lau <tlau9195@gmail.com>
2014-07-01 12:57:12 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -62,4 +62,16 @@
     return [CCBReader loadAsScene:@"MainScene"];
 }
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [...]
+    
+    [self setupCocos2dWithOptions:cocos2dSetup];
+    
+    [CCBReader load:@"Penguin"];
+    [CCBReader load:@"Seal"];
+    
+    return YES;
+}
+
 @end
